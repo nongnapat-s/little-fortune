0dc8a90a00c29a67092f3942156890f3b1ab3a16
nongnapat2017 <n.ngnapat@gmail.com>
2019-11-19 21:58:21 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Courgette&display=swap" rel="stylesheet">
+        <title>slothme.co</title>
+    </head>
+  <style>
+     body {
+        font-family: 'Courgette', cursive;
+        background-color: #A58ABF;
+        text-align: center;
+        color: #F5F6F8;
+    }
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 80px;
+    }
+
+    .links > a {
+        color: white;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+
+                    <img src="https://uppic.cc/d/5EKa" width=250 height=250/><br>
+                    Little fortune..
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
